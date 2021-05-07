ed4ab354c4b2be0bbd8bf4421019aabf8d21824c
Daniel Mitchell <danielmitchell@unomaha.com>
2021-05-06 20:35:03 -0500

    another change

--- sahbackend/models.py
+++ sahbackend/models.py
@@ -25,9 +25,9 @@ class Institution(models.Model):
         return str(self.institution_name)
 
 class Location(models.Model):
+    name = models.CharField(max_length=50)
     location_number = models.IntegerField(blank=False, null=False)
     institution_num = models.ForeignKey(Institution, on_delete=models.CASCADE, related_name='locationinst')
-    name = models.CharField(max_length=50)
     created_date = models.DateTimeField(
         default=timezone.now)
     updated_date = models.DateTimeField(auto_now_add=True)
@@ -41,10 +41,10 @@ class Location(models.Model):
         self.save()
 
     def __str__(self):
-        return str(self.location_number)
+        return str(self.name)
 
 class Volunteer(models.Model):
-    institution_num = models.ForeignKey(Institution, on_delete=models.CASCADE, related_name='volunteerinst')
+    institution_name = models.ForeignKey(Institution, on_delete=models.CASCADE, related_name='volunteerinst')
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=200)
