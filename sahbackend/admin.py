1c77746dc82109448d78782b389df9d2f9ed0ff6
Daniel Mitchell <danielmitchell@unomaha.com>
2021-05-06 20:40:11 -0500

    another change p3

--- sahbackend/admin.py
+++ sahbackend/admin.py
@@ -9,8 +9,8 @@ class InstitutionList(admin.ModelAdmin):
 
 class VolunteerList(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone')
-    list_filter = ['institution_num']
-    search_fields = ('first_name', 'last_name', 'institution_num')
+    list_filter = ['institution_name']
+    search_fields = ('first_name', 'last_name', 'institution_name')
     ordering = ['first_name']
 
 class LocationList(admin.ModelAdmin):
